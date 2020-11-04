dc15063ed8fa60fb9f3be4b90831052432688dd6
EliotVance <my.email@email.com>
2020-11-04 11:28:11 +0100

    complete sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import StickyHeader from './modules/StickyHeader'
 
-let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader()
 
+let mobileMenu = new MobileMenu()
 
 if (module.hot) {
   module.hot.accept()
