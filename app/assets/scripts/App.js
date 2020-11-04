195973931c9a0923f8f46dcb2565f19ce7ba3328
EliotVance <my.email@email.com>
2020-11-04 17:04:58 +0100

    complete the modal overlay for get in touch buttons

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,27 @@
-import '../styles/styles.css'
-import MobileMenu from './modules/MobileMenu'
-import StickyHeader from './modules/StickyHeader'
+import "../styles/styles.css";
+import MobileMenu from "./modules/MobileMenu";
+import StickyHeader from "./modules/StickyHeader";
 
-let stickyHeader = new StickyHeader()
+let stickyHeader = new StickyHeader();
+let mobileMenu = new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu()
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */"./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("There was an error."));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
