77ade39c8aba46b158885b71da1689f4967380da
EliotVance <my.email@email.com>
2020-11-05 18:14:12 +0100

    complete build-process configuration

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,28 +1,26 @@
-import "../styles/styles.css";
-import "lazysizes"
-import MobileMenu from "./modules/MobileMenu";
-import StickyHeader from "./modules/StickyHeader";
+import '../styles/styles.css'
+import 'lazysizes'
+import MobileMenu from './modules/MobileMenu'
+import StickyHeader from './modules/StickyHeader'
 
-let stickyHeader = new StickyHeader();
-let mobileMenu = new MobileMenu();
-let modal;
+new StickyHeader()
+new MobileMenu();
+let modal
 
-document.querySelectorAll(".open-modal").forEach((el) => {
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
     if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */"./modules/Modal")
-        .then((x) => {
-          modal = new x.default();
-          setTimeout(() => modal.openTheModal(), 20);
-        })
-        .catch(() => console.log("There was an error."));
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
     } else {
-      modal.openTheModal();
+      modal.openTheModal()
     }
-  });
-});
+  })
+})
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
 }
