45c68c311ec5d98e19e564eca88cffb41b3447e0
EliotVance <my.email@email.com>
2020-11-06 10:53:02 +0100

    React Testing

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,26 +1,40 @@
-import '../styles/styles.css'
-import 'lazysizes'
-import MobileMenu from './modules/MobileMenu'
-import StickyHeader from './modules/StickyHeader'
+import "../styles/styles.css";
+import "lazysizes";
+import MobileMenu from "./modules/MobileMenu";
+import StickyHeader from "./modules/StickyHeader";
 
-new StickyHeader()
+// React related codes
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Import React components
+import MyReactComponent from './modules/MyReactComponent'
+
+ReactDOM.render(
+  <MyReactComponent />,
+  document.querySelector("#my-react-example")
+);
+
+new StickyHeader();
 new MobileMenu();
-let modal
+let modal;
 
-document.querySelectorAll(".open-modal").forEach(el => {
-  el.addEventListener("click", e => {
-    e.preventDefault()
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
     if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
-        modal = new x.default()
-        setTimeout(() => modal.openTheModal(), 20)
-      }).catch(() => console.log("There was a problem."))
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("There was a problem."));
     } else {
-      modal.openTheModal()
+      modal.openTheModal();
     }
-  })
-})
+  });
+});
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
---
+++ app/assets/scripts/modules/MyReactComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function MyReactComponent() {
+  return (
+    <div className="page-section">
+      <div className="wrapper">
+        <h1 className="section-title section-title--blue">
+          This is My Amazing React Component
+        </h1>
+        <p>React is great and the sky is not green.</p>
+      </div>
+    </div>
+  );
+}
+
+export default MyReactComponent
